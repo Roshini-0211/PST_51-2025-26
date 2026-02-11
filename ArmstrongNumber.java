562262fd708809cc359d583e0d97986b08abde73
Roshini-0211 <156168425+Roshini-0211@users.noreply.github.com>
2026-02-11 12:12:37 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,52 @@
+package PST; 
+import java.util.Scanner;
+
+	
+
+	public class ArmstrongNumber {
+
+	    public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the Armstrong Number: ");
+	        int n = sc.nextInt();
+
+	        int temp = n;
+	        int count = 0;
+	        int sum = 0;
+
+	        
+	        while (temp > 0) {
+	            count++;
+	            temp = temp / 10;
+	        }
+
+	        temp = n; 
+
+	      
+	        while (temp > 0) {
+	            int digit = temp % 10;
+	            int power = 1;
+
+	           
+	            for (int i = 1; i <= count; i++) {
+	                power = power * digit;
+	            }
+
+	            sum = sum + power;
+	            temp = temp / 10;
+	        }
+
+	        
+	        if (sum == n) {
+	            System.out.println("This is Armstrong Number");
+	        } else {
+	            System.out.println("Not an Armstrong Number");
+	        }
+	        sc.close();
+	    }
+	}
+
+	
+
+
