6092daff8b8e72a460aaad8f0c91e5ca593cf9b3
Roshini-0211 <156168425+Roshini-0211@users.noreply.github.com>
2026-02-12 21:11:27 +0530

    Add files via upload

---
+++ Charter.java
@@ -0,0 +1,15 @@
+package PST;
+import java.util.Scanner;
+
+public class Charter {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the character is ");
+		String a =sc.next();
+		
+		System.out .println("the character is "+a);
+		
+		sc.close();
+	}
+	}
---
+++ ClassMethod.java
@@ -0,0 +1,45 @@
+package PST;
+
+public class ClassMethod {
+
+	public static void main(String[] args) {
+		CseB.anu();
+		CseB.Kasthuri();
+		CseB.abi();
+		CseB.amutha();
+		Cse2.abi();
+		Cse2.kasthuri();
+		Cse3.rocks();
+	}
+}
+class  CseB{
+	public static void anu(){
+		System.out.println("anu is my classmate ");
+	}
+	public static void Kasthuri(){
+		System.out.println("kasthuri is my deskmate ");
+	}
+	public static void abi(){
+		System.out.println("abi is my classmate ");
+	}
+	public static void amutha(){
+		System.out.println("amutha is my classmate ");
+	}
+		
+}
+ class Cse2{ 
+	 public static void kasthuri(){
+			System.out.println("kasthuri is my friend now ");
+		}
+	 public static void abi(){
+			System.out.println("abi is another class ");
+		}
+	
+}
+class Cse3{
+	public static void rocks(){
+		System.out.println("rocks is my friend ");
+	}
+	
+	
+}
