743c75b04f4d7bfd71e6ada442b469f2e52b2670
Roshini-0211 <156168425+Roshini-0211@users.noreply.github.com>
2026-02-13 20:37:22 +0530

    Add files via upload

---
+++ LargestThreeNumbers.java
@@ -0,0 +1,26 @@
+package PST;
+import java .util.Scanner;
+
+public class LargestThreeNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Ente the  Three Number : ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		
+			if (a >= b && a >= c) {
+	            System.out.println("Largest number is: " + a);
+	        } else if (b >= a && b >= c) {
+	            System.out.println("Largest number is: " + b);
+	        } else {
+	            System.out.println("Largest number is: " + c);
+	        }
+
+	        sc.close();
+		}
+
+	}
+
+
