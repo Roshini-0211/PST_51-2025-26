14207ab812d62e9e9b8075c4214f44d6924d2c6e
Roshini-0211 <156168425+Roshini-0211@users.noreply.github.com>
2026-02-13 20:41:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package PST;
+
+public class Main {
+
+	public static void main(String[] args) {
+		  method();
+		
+		}
+	public  static  void method()
+	{
+		Anu();
+		abi();
+		Banu();
+		Amutha();
+	}
+	public static  void Anu()
+	{
+		
+		System.out.println("  Anu is a beautiful girl");
+	}
+	public static void abi() {
+		System.out.println(" abi is a intelligent girl");
+	}
+	public static void Banu()
+	{
+		System.out.println(" Banu is a kind person");
+	}
+	public static void Amutha()
+	{
+		System.out.println(" Amutha is a Brave girl");
+	}
+	
+	
+	
+	
+
+}
