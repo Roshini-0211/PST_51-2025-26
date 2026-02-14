33a83284c6c9dd8786eb80d3c3584713154a2bee
Roshini-0211 <156168425+Roshini-0211@users.noreply.github.com>
2026-02-14 21:46:07 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package PST;
+import java .util.Scanner;
+public class Palindrome {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a word :");
+		String org=sc.next();
+		String rev="";
+		for(int i;i<org.length();i++) {
+			
+		}
+		if (org.charAt(0)==rev.charAt(0)) {
+			count++;
+		}
+		if (count==org.length());
+		
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
