4cc14e8006ba0a80b1847847c2f6a4b45f9d7c19
Roshini-0211 <156168425+Roshini-0211@users.noreply.github.com>
2026-02-16 21:08:07 +0530

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,27 @@
+package PST;
+import java.util.Scanner;
+public class Star {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the No of Star : ");
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		
+		
+	}
+
+}
+
+	public void Pattern(int num) {
+		int i,j;
+		for (int i=0;i<num;i++) {
+			for (int j=0;j<num;j++) {
+				System.out.print("*");
+				
+				
+			
+	}System.out.println(" ");
+		}
+		
+}
+
